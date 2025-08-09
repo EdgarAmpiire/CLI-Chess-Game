a5dd2518a9eeefb82b9111ca924d57956a7a466b
Edgar Ampiire <eampiire@gmail.com.com>
2025-08-09 21:19:57 +0300

    rook piece class logic

---
+++ lib/pieces/rook.rb
@@ -0,0 +1,28 @@
+class Rook < Piece
+  def symbol
+    color == :white ? "♖" : "♜"
+  end
+
+  def line_moves(board, from)
+    moves = []
+    [[1,0],[-1,0],[0,1],[0,-1]].each do |dr, dc| 
+      r,c = from
+      loop do
+        r += dr; c += dc
+        break unless r.between?(0,7) && c.between?(0, 7)
+        t = board[r, c] #board[r][c]
+        if t.nil?
+          moves << [r, c]
+        else
+          moves << [r, c] if enemy?(t)
+          break
+        end
+      end
+    end 
+    moves
+  end
+
+  def possible_moves(board, from)
+    line_moves(board, from)
+  end
+end
