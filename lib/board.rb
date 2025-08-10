3248d95f94857fe1f759c3d869be81cf00a0aec1
Edgar Ampiire <eampiire@gmail.com.com>
2025-08-10 13:52:37 +0300

    Piece setup logic

--- lib/board.rb
+++ lib/board.rb
@@ -57,5 +57,31 @@ class Board
     end
   end
 
-  
+  def setup_pieces
+    # Pawns
+    8.times do |c|
+      self[1,c] = Pawn.new(:black)
+      self[6,c] = Pawn.new(:white)
+    end
+
+    # Rooks
+    self[0,0] = Rook.new(:black); self[0,7] = Rook.new(:black)
+    self[7,0] = Rook.new(:white); self[7,7] = Rook.new(:white)
+
+    # Knights
+    self[0,1] = Knight.new(:black); self[0,6] = Knight.new(:black)
+    self[7,1] = Knight.new(:white); self[7,6] = Knight.new(:white)
+
+    # Bishops
+    self[0,2] = Bishop.new(:black); self[0,5] = Bishop.new(:black)
+    self[7,2] = Bishop.new(:white); self[7,5] = Bishop.new(:white)
+
+    # Queens
+    self[0,3] = Queen.new(:black)
+    self[7,3] = Queen.new(:white)
+
+    # Kings
+    self[0,4] = King.new(:black)
+    self[7,4] = King.new(:white)
+  end
 end
