4a619e442d3749b1dc8ff28fbfd446b1a47c39e0
Edgar Ampiire <eampiire@gmail.com.com>
2025-08-11 20:25:02 +0300

    Feature: Castling rule

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -15,6 +15,48 @@ class King < Piece
         moves << [nr, nc] if target.nil? || enemy?(target)
       end
     end
+
+    # Castling
+    moves.concat(castling_moves(board,from))
+
     moves
+  end
+
+  private
+
+  def castling_moves(board, from)
+    return [] if moved # King has moved -> No Castling
+
+    r, c = from 
+    castling = []
+
+    # King side Castling (right rook)
+    if can_castle?(board, r, c, 7)
+      castling << [r, c + 2] 
+    end
+
+    # Queen side Castling (left rook)
+    if can_castle?(board, r, c, 0)
+      castling << [r, c - 2]
+    end
+
+    castling
+  end
+
+  def can_castle?(board, r, c, rook_col)
+    rook = board[r, rook_col]
+    return false unless rook.is_a?(Rook) && !rook.moved && rook.color == color
+
+    step = rook_col > c ? 1 : -1
+    col_range = rook_col > c ? (c + 1...rook_col) : (rook_col + 1...c)
+
+    # Check if squares between are empty
+    return false unless col_range.all? { |col| board[r, col].nil? }
+
+    # Check that the king is not in check and doesn't pass through check
+    [c, c + step, c + 2 * step].each do |col|
+      return false if board.square_attacked?([r, col], color)
+    end
+    true
   end
 end
